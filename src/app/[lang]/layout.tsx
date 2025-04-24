4479d99301376475c7d706bc612114ed2c5ec0a2
bnjmn <benja_e_e@hotmail.com>
2025-04-23 20:22:14 -0400

    try(intentando añadir multi langs

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next";
 
 /* styles */
-import "./globals.css";
+import "../globals.css";
 import "react-toastify/dist/ReactToastify.css";
 
 /* fonts */
@@ -14,16 +14,21 @@ import { unstable_ViewTransition as ViewTransition } from "react";
 
 export const metadata: Metadata = {
   title: "Genomas",
-  description: "GENOMAS offers a centralized platform for all your genomic data, ensuring your analyses are always synchronized, accessible, and secure.",
+  description:
+    "GENOMAS offers a centralized platform for all your genomic data, ensuring your analyses are always synchronized, accessible, and secure.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
-}: Readonly<{
+  params,
+}: {
   children: React.ReactNode;
-}>) {
+  params: { lang: string };
+}) {
+  const { lang } = params;
+
   return (
-    <html lang="en">
+    <html lang={`${lang}`}>
       <body className={`${inter} antialiased`}>
         <ViewTransition name="page">{children}</ViewTransition>
         <ToastContainer position="top-right" autoClose={5000} />
