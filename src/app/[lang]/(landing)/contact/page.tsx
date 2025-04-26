3ff3f39695d7e07ac7a900c8c836d9d1cafd98fe
bnjmn <benja_e_e@hotmail.com>
2025-04-25 20:15:43 -0400

    trabajando pagina de contact

--- src/app/[lang]/(landing)/contact/page.tsx
+++ src/app/[lang]/(landing)/contact/page.tsx
@@ -3,70 +3,140 @@ import "./page.css";
 export default function page() {
   return (
     <section className="contact--page">
-      <article>
-        <figure>
-          <img
-            src="/images/contact/contact.svg"
-            alt="Contact Us"
-            className="contact--page__image"
-          />
-          <figcaption className="contact--page__caption">Contact Us</figcaption>
-        </figure>
-
-        <div>
-          <span className="section-icon" aria-hidden="true">
-            🤔
-          </span>
-          <h2 className="section-title">Frequently asked questions</h2>
-        </div>
-        <div className="questions-and-answers">
-          <details>
-            <summary>Can I animate details/summary elements?</summary>
-            <p>
-              This used to be something that was impossible, but now thanks to
-              modern CSS, it's actually pretty easy to do!
-            </p>
-          </details>
-
-          <details>
-            <summary>How is it possible?</summary>
-            <p>
-              There are a few different ways that you can do it. One of them is
-              using <code>calc-size()</code>, which has some extra things you
-              could do with it, but right now the easiest way is to use a
-              combination of <code>interpolate-size: allow-keywords;</code> and{" "}
-              <code>transition-behavior: allow-discrete</code>.
-            </p>
-          </details>
-
-          <details>
-            <summary>What about browser support?</summary>
-            <p>
-              Browser support isn't great as of the time of creating this demo,
-              but it'll only get better from here. Also, it is a great{" "}
-              <em>progressive enhancement</em>. This means that it won't break
-              anything for people on older browsers, they just won't get the
-              fancy animation you've created, while people on newer browser will
-              get it!
-            </p>
-          </details>
-
-          <details>
-            <summary>What's the catch?</summary>
-            <p>
-              There is none! It's easy to do and it works great! I feel like
-              that's too short, and I want to use this demo to show the
-              animations and need a longer box to show it properly so here's a
-              little bit of extra filler text lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Modi at ipsum dolor tempore impedit
-              corrupti ad fugiat voluptate reiciendis voluptatum!
-            </p>
-          </details>
-        </div>
-      </article>
-
-      <article></article>
-      <article></article>
+      <AboutUs />
+      <OurStory />
+      <Team />
     </section>
   );
 }
+
+export const AboutUs = () => {
+  return (
+    <article>
+      <figure>
+        <img
+          src="/images/contact/contact.svg"
+          alt="Contact Us"
+          className="contact--page__image"
+        />
+        <figcaption className="contact--page__caption">
+          Contact Us
+        </figcaption>
+      </figure>
+
+      <div>
+        <span className="section-icon" aria-hidden="true">
+          🤔
+        </span>
+        <h2 className="section-title">Frequently asked questions</h2>
+      </div>
+      <div className="questions-and-answers">
+        <details>
+          <summary>Can I animate details/summary elements?</summary>
+          <p>
+            This used to be something that was impossible, but now thanks to
+            modern CSS, it's actually pretty easy to do!
+          </p>
+        </details>
+
+        <details>
+          <summary>How is it possible?</summary>
+          <p>
+            There are a few different ways that you can do it. One of them is
+            using <code>calc-size()</code>, which has some extra things you
+            could do with it, but right now the easiest way is to use a
+            combination of <code>interpolate-size: allow-keywords;</code> and{" "}
+            <code>transition-behavior: allow-discrete</code>.
+          </p>
+        </details>
+
+        <details>
+          <summary>What about browser support?</summary>
+          <p>
+            Browser support isn't great as of the time of creating this demo,
+            but it'll only get better from here. Also, it is a great{" "}
+            <em>progressive enhancement</em>. This means that it won't break
+            anything for people on older browsers, they just won't get the fancy
+            animation you've created, while people on newer browser will get it!
+          </p>
+        </details>
+
+        <details>
+          <summary>What's the catch?</summary>
+          <p>
+            There is none! It's easy to do and it works great! I feel like
+            that's too short, and I want to use this demo to show the animations
+            and need a longer box to show it properly so here's a little bit of
+            extra filler text lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Modi at ipsum dolor tempore impedit corrupti ad fugiat
+            voluptate reiciendis voluptatum!
+          </p>
+        </details>
+      </div>
+    </article>
+  );
+};
+
+export const OurStory = () => {
+  return (
+    <article className="our-story">
+      <header>
+        <h1>Our History</h1>
+        <p>I'm Karen, founder Genomas, and this is our history </p>
+      </header>
+      <ul className="timeline">
+        <li className="container">
+          <img src="../../columns/columnsGenshin/assets/7.png" alt="" />
+          <div className="text-box">
+            <h2>Historia de la Empresa</h2>
+            <dl>
+              <dt>Feb 2015</dt>
+              <dd>Fundación de la empresa.</dd>
+            </dl>
+            <span className="arrow"></span>
+          </div>
+        </li>
+
+        <li className="container">
+          <img src="../../columns/columnsGenshin/assets/7.png" alt="" />
+          <div className="text-box">
+            <h2>Historia de la Empresa</h2>
+            <dl>
+              <dt>Feb 2015</dt>
+              <dd>Fundación de la empresa.</dd>
+            </dl>
+            <span className="arrow"></span>
+          </div>
+        </li>
+
+        <li className="container">
+          <img src="../../columns/columnsGenshin/assets/7.png" alt="" />
+          <div className="text-box">
+            <h2>Historia de la Empresa</h2>
+            <dl>
+              <dt>Feb 2015</dt>
+              <dd>Fundación de la empresa.</dd>
+            </dl>
+            <span className="arrow"></span>
+          </div>
+        </li>
+
+        <li className="container">
+          <img src="../../columns/columnsGenshin/assets/7.png" alt="" />
+          <div className="text-box">
+            <h2>Historia de la Empresa</h2>
+            <dl>
+              <dt>Feb 2015</dt>
+              <dd>Fundación de la empresa.</dd>
+            </dl>
+            <span></span>
+          </div>
+        </li>
+      </ul>
+    </article>
+  );
+};
+
+export const Team = () => {
+  return <article></article>;
+};
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,58 +1,20 @@
+/* components */
 import TeamCarrousel from "@/components/sections/section5/TeamCarrousel";
 import Section1 from "@/components/sections/Section1";
 import Section2 from "@/components/sections/Section2";
 import Section3 from "@/components/sections/Section3";
 import Section4 from "@/components/sections/Section4";
-import "./page.css";
 import HeaderLanding from "@/components/headers/HeaderLanding";
 import { FooterLanding } from "@/components/footer/FooterLanding";
 
-const squares = Array.from({ length: 15 }, (_, i) => i);
-
-const colors = [
-  "rgba(255, 0, 0, 0.6)", // Rojo
-  "rgba(0, 255, 0, 0.6)", // Verde
-  "rgba(0, 0, 255, 0.6)", // Azul
-  "rgba(255, 255, 0, 0.6)", // Amarillo
-  "rgba(255, 0, 255, 0.6)", // Magenta
-  "rgba(0, 255, 255, 0.6)", // Cyan
-];
+/* styles */
+import "./page.css";
 
 export default function Home() {
   return (
     <>
       <div className="layout">
         <HeaderLanding />
-        {/* <div className="box">
-          {squares.map((i) => {
-            const side = Math.random() < 0.5 ? "left" : "right"; // Lado aleatorio
-            const randomY = Math.random() * 100; // Posición aleatoria en Y (vh)
-            const offsetX = Math.random() * 10 + 5; // Margen fuera del contenido
-            const duration = Math.random() * 5 + 5; // Entre 5s y 10s
-            const delay = Math.random() * 5; // Hasta 5s de retraso
-            const color = colors[Math.floor(Math.random() * colors.length)]; // Color aleatorio
-
-            return (
-              <div
-                key={i}
-                className="square"
-                style={
-                  {
-                    "--i": i,
-                    "--y": `${randomY}vh`,
-                    "--x":
-                      side === "left"
-                        ? `calc(50% - 640px - ${offsetX}px)`
-                        : `calc(50% + 640px + ${offsetX}px)`,
-                    "--duration": `${duration}s`,
-                    "--delay": `${delay}s`,
-                    "--color": color,
-                  } as React.CSSProperties
-                }
-              ></div>
-            );
-          })}
-        </div> */}
         <Section1 />
         <Section2 />
         <Section3 />
